9f80e8eae9cc2eb1b0f5ed6f23a1cd3ec0c79f9f
Pawel Cichon <Pawel.Cichon@empik.com>
2020-08-07 11:41:07 +0200

    git init

---
+++ src/main/kotlin/com/currency/account/service/CurrencyAccountServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.currency.account.service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CurrencyAccountServiceApplication
+
+fun main(args: Array<String>) {
+  runApplication<CurrencyAccountServiceApplication>(*args)
+}
